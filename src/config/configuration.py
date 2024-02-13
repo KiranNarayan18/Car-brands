ed702c45977e7dcb383b326ebd1d1409ee50ef4a
Kiran Narayan <kiran.narayan71@gmail.com>
2024-02-13 08:33:13 +0530

    entity and configuration

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -5,7 +5,8 @@ from src.logger import logger, CustomException
 from src.utils import read_yaml_file
 from src.constants import *
 from src.entity import (
-                            DataIngestionConfig
+                            DataIngestionConfig,
+                            ModelTrainingConfig
                         )
 
 class ConfigurationManager:
@@ -31,6 +32,22 @@ class ConfigurationManager:
             return data_ingestion_config
 
         except Exception as error:
-            logger.error(error)
+            logger.error(CustomException(error, sys))
         
 
+    def ModelTraining(self):
+
+        try:
+            config = self.config.model_training
+
+            os.makedirs(config.model_dir, exist_ok=True)
+
+            model_training = ModelTrainingConfig(
+                root_dir=config.root_dir,
+                model_dir=config.model_dir
+            )
+
+            return model_training
+        
+        except Exception as error:
+            logger.error(CustomException(error, sys))
--- src/entity/__init__.py
+++ src/entity/__init__.py
@@ -7,3 +7,7 @@ class DataIngestionConfig:
     url: str
     root_dir: Path
 
+@dataclass(frozen=True)
+class ModelTrainingConfig:
+    root_dir: Path
+    model_dir: Path
