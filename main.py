983383970575db2e96b155d3c05707924f51f174
Kiran Narayan <kiran.narayan71@gmail.com>
2024-02-14 07:54:42 +0530

    train main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.logger import logger, CustomException
 
 
 from src.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.pipeline.model_training_pipeline import ModelTrainingPipeline
 
 if __name__ == "__main__":
     
@@ -13,6 +14,14 @@ if __name__ == "__main__":
         data_ingestion_obj = DataIngestionPipeline()
         data_ingestion_obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+
+
+        STAGE_NAME = 'Model Training'
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        model_training_obj = ModelTrainingPipeline()
+        model_training_obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+
 
 
 
