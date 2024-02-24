470f56124763754e78066d39f28b9fad0b1beaac
Kiran Narayan <kiran.narayan71@gmail.com>
2024-02-24 22:14:15 +0530

    updated main

--- main.py
+++ main.py
@@ -2,34 +2,34 @@ import sys
 from src.logger import logger, CustomException
 
 
-# from src.pipeline.data_ingestion_pipeline import DataIngestionPipeline
-# from src.pipeline.model_training_pipeline import ModelTrainingPipeline
-from src.config.configuration import ConfigurationManager
+from src.pipeline.data_ingestion_pipeline import DataIngestionPipeline
+from src.pipeline.model_training_pipeline import ModelTrainingPipeline
+# from src.config.configuration import ConfigurationManager
 
-from src.components.model_training_pytorch import ModelTrainingPytorch
+# from src.components.model_training_pytorch import ModelTrainingPytorch
 
 if __name__ == "__main__":
     
     try:
 
-        config = ConfigurationManager()
-        config = config.ModelTraining()
+        # config = ConfigurationManager()
+        # config = config.ModelTraining()
 
-        obj = ModelTrainingPytorch(config)
-        obj.train()
+        # obj = ModelTrainingPytorch(config)
+        # obj.train()
 
-        # STAGE_NAME = 'Data Ingestion'
-        # logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        # data_ingestion_obj = DataIngestionPipeline()
-        # data_ingestion_obj.main()
-        # logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+        STAGE_NAME = 'Data Ingestion'
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_ingestion_obj = DataIngestionPipeline()
+        data_ingestion_obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 
 
-        # STAGE_NAME = 'Model Training'
-        # logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        # model_training_obj = ModelTrainingPipeline()
-        # model_training_obj.main()
-        # logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+        STAGE_NAME = 'Model Training'
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        model_training_obj = ModelTrainingPipeline()
+        model_training_obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 
 
 
